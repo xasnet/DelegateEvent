d1372494c6b28f7a240eb04321dd2d5cd452bcea
xasnet <xasnet@gmail.com>
2023-01-28 14:51:13 +0300

    DE-1.Added logic

---
+++ src/DelegatesEvents/Extensions/EnumerableExt.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+
+namespace DelegatesEvents.Extensions;
+
+public static partial class EnumerableExt
+{
+    public static T GetMax<T>(this IEnumerable e, Func<T, float> getParameter)
+        where T : class
+    {
+        if (e == null)
+        {
+            throw new Exception("No element");
+        }
+
+        if (getParameter == null)
+        {
+            throw new Exception("No function");
+        }
+
+        T value;
+
+        var el = e.GetEnumerator();
+
+        if (!el.MoveNext())
+        {
+            throw new Exception("No elements");
+        }
+
+        value = (T)el.Current;
+
+        if (!el.MoveNext())
+        {
+            return value;
+        }
+
+        T x = (T)el.Current;
+
+        if (getParameter(x) > getParameter(value))
+        {
+            value = x;
+        }
+
+        while (el.MoveNext())
+        {
+            x = (T)el.Current;
+
+            if (getParameter(x) > getParameter(value))
+            {
+                value = x;
+            }
+        }
+
+        return value;
+    }
+}
---
+++ src/DelegatesEvents/FileWork/FileActions.cs
@@ -0,0 +1,9 @@
+﻿namespace DelegatesEvents.FileWork;
+
+public class FileActions
+{
+    public void FindFile(FileInfo fileInfo)
+    {
+        Console.WriteLine(fileInfo.FullName);
+    }
+}
